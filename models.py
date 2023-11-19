ba0e6864fe1e6a1839c39c9d2ce1af0e73a27ec9
greenavoca <pawel.gronski@vp.pl>
2023-11-19 21:39:26 +0000

    add association proxy

--- models.py
+++ models.py
@@ -1,23 +1,27 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from database import Base
+from sqlalchemy.orm import relationship
 
+
+AuthorSong = Table('authorsong',
+                    Base.metadata,
+                    Column('id', Integer, primary_key=True, index=True),
+                    Column('authorId', Integer, ForeignKey('author.id')),
+                    Column('songId', Integer, ForeignKey('song.id'))
+                   )
 
 class Author(Base):
     __tablename__ = 'author'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    songs = relationship('Song', secondary=AuthorSong, backref='Author')
 
 class Song(Base):
     __tablename__ = 'song'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    authors = relationship('Author', secondary=AuthorSong, backref='Song')
 
-class AuthorSong(Base):
-    __tablename__ = 'authorsong'
-
-    id = Column(Integer, primary_key=True, index=True)
-    author = Column(ForeignKey('author.id'))
-    song = Column(ForeignKey('song.id'))
 
