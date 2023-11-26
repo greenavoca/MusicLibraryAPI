a91963edefbccd9ec31961e73e3bca9b411483ee
greenavoca <pawel.gronski@vp.pl>
2023-11-26 20:17:59 +0000

    add DELETE method

--- main.py
+++ main.py
@@ -40,18 +40,18 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.post('/create-song/')
 async def create_song(song: SongBase, db: db_dependency):
-    q_author = db.query(models.Author).filter_by(name=song.author).first()
-    q_title = db.query(models.Song).filter_by(title=song.title).first()
+    q_author = db.query(models.Author).filter_by(name=song.author.lower()).first()
+    q_title = db.query(models.Song).filter_by(title=song.title.lower()).first()
     if q_author is not None and q_title is not None:
         q_song = db.query(models.AuthorSong).filter_by(authorId=q_author.id, songId=q_title.id).first()
         if q_song is not None:
             raise HTTPException(403, detail='Already exists!')
     db_author = q_author
     if q_author is None:
-        db_author = models.Author(name=song.author)
+        db_author = models.Author(name=song.author.lower())
     db_title = q_title
     if q_title is None:
-        db_title = models.Song(title=song.title)
+        db_title = models.Song(title=song.title.lower())
     db_author.songs.append(db_title)
     db.add_all([db_author, db_title])
     db.commit()
@@ -72,7 +72,7 @@ async def fetch_all_songs(db: db_dependency):
 
 @app.get('/find-title/')
 async def find_title(author: FindSongBase, db: db_dependency):
-    q = db.query(models.Author).filter_by(name=author.author).first()
+    q = db.query(models.Author).filter_by(name=author.author.lower()).first()
     songs_list = []
     if q is None:
         return HTTPException(404, detail='Author not found!')
@@ -82,7 +82,7 @@ async def find_title(author: FindSongBase, db: db_dependency):
 
 @app.get('/find-author/')
 async def find_author(song: FindAuthorBase, db: db_dependency):
-    q = db.query(models.Song).filter_by(title=song.title).first()
+    q = db.query(models.Song).filter_by(title=song.title.lower()).first()
     authors_list = []
     if q is None:
         return HTTPException(404, detail='Author not found!')
@@ -96,7 +96,7 @@ async def update_author(author_id: int, author: AuthorUpdateBase, db: db_depende
     if q is None:
         return HTTPException (404, detail='Author not found!')
     if author.new_name is not None:
-        q.name = author.new_name
+        q.name = author.new_name.lower()
         db.commit()
     return {'Author updated!'}
 
@@ -106,6 +106,18 @@ async def update_title(title_id: int, title: TitleUpdateBase, db: db_dependency)
     if q is None:
         return HTTPException (404, detail='Title not found!')
     if title.new_title is not None:
-        q.title = title.new_title
+        q.title = title.new_title.lower()
         db.commit()
     return {'Title updated!'}
+
+@app.delete('/delete-song/')
+async def delete_song(song: SongBase, db: db_dependency):
+    q = db.query(models.Author).filter_by(name=song.author.lower()).first()
+    if q is None:
+        return HTTPException(404, detail='Song not found!')
+    to_delete = [item for item in q.songs if item.title == song.title.lower()]
+    if not to_delete:
+        return HTTPException(404, detail='Song not found!')
+    q.songs.remove(to_delete[0])
+    db.commit()
+    return {'Song deleted!'}
